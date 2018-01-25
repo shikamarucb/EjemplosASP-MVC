00d2c6c876b594422efe9c413d4fa2129e387473
Andres Camacho <cbandresf@hotmail.com>
2018-01-25 18:38:58 -0500

    Query String Ejemplo
    Se muestra como mandar un query desde la url a un action de un controlador

---
+++ EjemplosASP/QueryStringAction/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QueryStringAction.Controllers
+{
+    public class HomeController : Controller
+    {
+        //Query String ej: nombre=felipe
+        //tener cuidado con no pasar un tipo por valor, al no especificar un query string
+        //cuando se llama la url, se envian valores null como parametros y no es posible asignar
+        //null a un tipo por valor
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //Edad al ser un nullable no tiene problema al recibir un null
+        public ActionResult About(string nombre, int? edad)
+        {
+            if(edad.HasValue)
+                ViewBag.Message = "Your application description page. " + nombre + " "
+                    + edad.Value;
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ EjemplosASP/QueryStringAction/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="QueryStringAction.MvcApplication" Language="C#" %>
