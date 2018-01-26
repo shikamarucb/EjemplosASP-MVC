8a2614f88e0c782a9f5d7dd4bd6a49fdbb7a01d4
Andres Camacho <cbandresf@hotmail.com>
2018-01-26 01:11:04 -0500

    RenderAction y Action Ejemplos
    Mirar las vistas del HomeController y el HomeController

---
+++ EjemplosASP/RenderAction y Action/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RenderAction_y_Action.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        //ChildActionOnly indica que no podemos acceder directamente al metodo, sino desde
+        //una accion "hija"
+        [ChildActionOnly]
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ EjemplosASP/RenderAction y Action/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RenderAction_y_Action.MvcApplication" Language="C#" %>
