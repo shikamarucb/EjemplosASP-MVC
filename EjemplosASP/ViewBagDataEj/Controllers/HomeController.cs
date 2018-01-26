b90c532b28d0e682c96a8959951973c7e775561b
Andres Camacho <cbandresf@hotmail.com>
2018-01-25 22:58:33 -0500

    ViewBag y ViewData Ejemplos

---
+++ EjemplosASP/ViewBagDataEj/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ViewBagDataEj.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            //Parametros que se envian a la vista
+            ViewBag.mensaje = "Hola mundo";
+            ViewBag.fecha = new DateTime();
+            ViewData["numero"] = 30;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
