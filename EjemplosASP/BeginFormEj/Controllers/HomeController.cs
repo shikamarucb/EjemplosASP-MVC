c60151366b54dba090bd4e985d28af12b56e93c4
Andres Camacho <cbandresf@hotmail.com>
2018-01-28 01:42:18 -0500

    BeginFrom Ejemplos

---
+++ EjemplosASP/BeginFormEj/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BeginFormEj.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Persona persona)
+        {
+            ViewBag.visajes = persona.Nombre + persona.Edad;
+            return View(persona);
+        }
+
+    }
+
+    public class Persona {
+        public int Edad { get; set; }
+        public string Nombre { get; set; }
+    }
+}
---
+++ EjemplosASP/BeginFormEj/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BeginFormEj.MvcApplication" Language="C#" %>
