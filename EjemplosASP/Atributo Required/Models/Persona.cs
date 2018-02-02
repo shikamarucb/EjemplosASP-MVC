639ab8bb6891df4b332dda5abc23dbbee3b65573
Andres Camacho <cbandresf@hotmail.com>
2018-02-01 21:01:41 -0500

    Atributo StringLength

--- EjemplosASP/Atributo Required/Models/Persona.cs
+++ EjemplosASP/Atributo Required/Models/Persona.cs
@@ -9,6 +9,10 @@ namespace Atributo_Required.Models
     public class Persona
     {
         public int Id { get; set; }
+
+        //Atributo StringLength: Longitud minima y maxima del campo "Nombre"
+        [StringLength(50,MinimumLength = 5,ErrorMessage ="El campo {0} debe tener minimo {2} caracteres y maximo {1}" +
+            " caracteres")]
         public string Nombre { get; set; }
 
         //Atributo Required: El atributo "Apellido" es obligatorio para crear un nuevo modelo de "Persona" en la BD
