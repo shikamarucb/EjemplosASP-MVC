2d152ae1f307c6c050240b7eb2be47869c45e05d
Andres Camacho <cbandresf@hotmail.com>
2018-02-17 00:40:47 -0500

    Multi idioma

---
+++ EjemplosASP/MultiIdioma/Models/Persona.cs
@@ -0,0 +1,22 @@
+﻿using Recursos;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MultiIdioma.Models
+{
+    public class Persona
+    {
+        public int Id { get; set; }
+
+        //Se crean los archivos de recurso con nombre igual, lo unico que varia es el
+        //Table of Language Culture Names, Codes, que va seguido despues de un punto
+        //Los archivos de recursos se crearon en la carpeta Recursos
+
+        [Display(ResourceType =typeof(Persona_res), Name ="Persona_Nombre_Mostrar" )]
+        [Required(ErrorMessageResourceType =typeof(Persona_res),ErrorMessageResourceName ="Persona_Nombre_Error")]
+        public string Nombre { get; set; }
+    }
+}
---
+++ EjemplosASP/MultiIdioma/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MultiIdioma.Startup))]
+namespace MultiIdioma
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
