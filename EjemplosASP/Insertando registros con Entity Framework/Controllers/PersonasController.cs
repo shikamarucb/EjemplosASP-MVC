4479b2b29c1e87c23b37a84613c1989374388e7d
Andres Camacho <cbandresf@hotmail.com>
2018-02-18 23:05:30 -0500

    Actualizar registro Entity Framework
    ver metodo edit del controlador

--- EjemplosASP/Insertando registros con Entity Framework/Controllers/PersonasController.cs
+++ EjemplosASP/Insertando registros con Entity Framework/Controllers/PersonasController.cs
@@ -86,20 +86,32 @@ namespace Insertando_registros_con_Entity_Framework.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Edit([Bind(Include = "Id,Nombre,Apellido,Nacimiento,Edad")] Persona persona)
         {
+            //Metodos para editar en EF
+
+            //Metodo 1
+            var personaEditar = db.Personas.FirstOrDefault(x => x.Id == 1);
+            personaEditar.Nombre = "Metodo1";
+            personaEditar.Edad = 51;
+            db.SaveChanges();
+
+            //Metodo 2: Actualizacion Parcial
+            var personaEditar2 = new Persona();
+            personaEditar2.Id = 2;
+            personaEditar2.Nombre = "Metodo2";
+            //Este campo no se va a modificar
+            personaEditar2.Edad = 52;
+            db.Personas.Attach(personaEditar2);
+            db.Entry(personaEditar2).Property(x => x.Nombre).IsModified = true;
+            db.SaveChanges();
+
+            //Metodo 3 actualizado desde la vista
             if (ModelState.IsValid)
             {
-                var listaPersonas = new List<Persona>() { persona};
-
-                listaPersonas.Add(new Persona() { Nombre="Felipe", Apellido="Barragan", Edad=32, Nacimiento=new DateTime(1995,12,15)});
-
                 db.Entry(persona).State = EntityState.Modified;
-
-                //Asi se añaden multiples registro a la DB
-                db.Personas.AddRange(listaPersonas);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            return View(persona);
+            return RedirectToAction("Index");
         }
 
         // GET: Personas/Delete/5
